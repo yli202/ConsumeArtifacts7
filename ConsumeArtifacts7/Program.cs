7495e0dccd13c082a818471d9c4e067804d43549
Narendra <Narendra@DESKTOP-C03GSGC>
2022-12-01 08:29:02 -0500

    Add project files.

---
+++ ConsumeArtifacts7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsumeArtifacts7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo consume Azure artifact");
+
+            AlgebraClassLibrary.Algebra algebra = new AlgebraClassLibrary.Algebra();
+
+            Console.WriteLine($"10+10={algebra.Addition(10, 10)}");
+        }
+    }
+}
